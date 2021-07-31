34daea39ed51aa8ff7fb48e50ed4aae239c74349
Mustafa Alp  Çetin <mustafaalpcetin1@gmail.com>
2021-07-31 18:22:06 +0300

    Revert "Revert "Hello World Again""
    This reverts commit fdad52dce3573493208af126ac8fa25a53f72da2.

--- src/main/java/com/kentarsivi/controller/v1/api/UserController.java
+++ src/main/java/com/kentarsivi/controller/v1/api/UserController.java
@@ -87,7 +87,7 @@ public class UserController {
 
 		if (responseDocumentDtos.getSize() <= 0)
 			throw new DocumentNotFoundException("DOCUMENT NOT FOUND");
-
+		System.out.println("Hello World!");
 		return new ResponseEntity<Page<DocumentDto>>(responseDocumentDtos, HttpStatus.OK);
 	}
 
